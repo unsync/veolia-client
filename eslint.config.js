58cc41e2c55558ab060d6319aa926f7a2c9792ca
unsync <1211591+unsync@users.noreply.github.com>
2026-02-07 11:04:38 +0100

    chore(lint): customize ESLint config and update version
    Turns off specific import rules to reduce false positives and adds no-warn-ignored flag to lint-staged for cleaner lint fixes.
    
    Bumps package version to 1.0.11 to reflect config update.

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,9 @@
 import antfu from '@antfu/eslint-config'
 
-export default antfu()
+export default antfu({
+  rules: {
+    'import/no-duplicates': 'off',
+    'import/no-self-import': 'off',
+    'import/order': 'off',
+  },
+})
